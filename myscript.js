313452ba333f0e9118222ee2c4a692f6913eae1e
Alexander Sabani <sabani@loidl-consulting.at>
2023-02-14 15:28:41 +0100

    now working

--- myscript.js
+++ myscript.js
@@ -1,11 +1,12 @@
-var link = document.getElementById("githubfetcher").innerHTML;
-
 fetch('https://raw.githubusercontent.com/alexander1220/alexander1220/main/README.md').then(function (response) {
     return response.text();
 }).then(function (html) {
     var parser = new DOMParser();
     var doc = parser.parseFromString(html, 'text/html');
-    link = doc.body.innerHTML;
+
+    const newMD = document.createElement("md-block");
+    newMD.innerHTML = doc.body.innerHTML;
+    document.getElementById('maincontent').appendChild(newMD);
 }).catch(function (err) {
     console.warn('Something went wrong.', err);
 });
